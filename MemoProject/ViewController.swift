fc255667e7cea7df50b2320b1f66a994fbf1437a
seongahshin <90595710+seongahshin@users.noreply.github.com>
2022-08-31 17:33:44 +0900

    Initial Commit

---
+++ MemoProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemoProject
+//
+//  Created by 신승아 on 2022/08/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
