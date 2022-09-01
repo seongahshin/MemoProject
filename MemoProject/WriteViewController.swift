6fdff0ac65a95cc79766b672cfe42bbc6433c31d
seongahshin <90595710+seongahshin@users.noreply.github.com>
2022-09-01 22:52:26 +0900

    WriteViewController UI & ViewController Transition

--- MemoProject/WriteViewController.swift
+++ MemoProject/WriteViewController.swift
@@ -5,14 +5,37 @@
 //  Created by 신승아 on 2022/08/31.
 //
 
-import Foundation
 import UIKit
+import SnapKit
 
 class WriteViewController: UIViewController {
+    
+    var textView: UITextView = {
+        let view = UITextView()
+        return view
+    }()
+    
+    
+    @objc func rightbarButtonItemClicked() {
+        print(#function)
+        self.navigationController?.popViewController(animated: true)
+    }
     
     override func viewDidLoad() {
         self.navigationController?.navigationBar.prefersLargeTitles = true
         self.navigationItem.largeTitleDisplayMode = .always
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "완료", style: .done, target: self, action: #selector(rightbarButtonItemClicked))
+        configureUI()
+    }
+    
+    func configureUI() {
+        [textView].forEach {
+            view.addSubview($0)
+        }
+        
+        textView.snp.makeConstraints { make in
+            make.top.left.right.bottom.equalTo(0)
+        }
     }
     
     
